35bd72d8e103d38e3983f82c6bd3591a30f6b51d
Steven Kristian <stevenkristian300900@gmail.com>
2025-01-29 00:52:13 +0700

    added lorem-ipsum for testing component

---
+++ src/app/(dashboard)/ui/components/lorem-ipsum.tsx
@@ -0,0 +1,19 @@
+export default function LoremIpsum() {
+  return (
+    <main>
+      <h1>h1: Welcome to Imagination Company</h1>
+      <h2>h2: Provides an implementation for your imagination</h2>
+      <h3>h3: Provides smaller conent</h3>
+      <p>
+        p: Our platform makes you realize that your imagination is no longer a
+        thought, but a matter that can be used.
+      </p>
+      <ol>
+        <li>li: Step 1: Sign up for a free account</li>
+        <li>li: Step 2: Write your imagination</li>
+        <li>li: Step 3: Explore the progress of your imagination</li>
+      </ol>
+      <button>button: Start Free Trial</button>
+    </main>
+  );
+}
